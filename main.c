4f8c6010e89fbd142f1c8989fdf036807ca1860f
unknown <caesar>
2017-12-11 15:40:15 +0800

    可用模板

---
+++ main.c
@@ -0,0 +1,42 @@
+#include"main.h"
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("NIC test");
+MODULE_AUTHOR("MAC");
+
+static struct nf_hook_ops preRoutHook;
+
+int mmHookInit(void)
+{
+	preRoutHook.hook = preRoutHookDisp;
+	preRoutHook.hooknum = NF_INET_PRE_ROUTING;
+	preRoutHook.pf = NFPROTO_IPV4;
+	preRoutHook.priority = NF_IP_PRI_LAST;
+	nf_register_hook(&preRoutHook);
+	return 0;
+}
+
+
+void mmHookExit(void)
+{
+	nf_unregister_hook(&preRoutHook);
+}
+
+
+static int mm_init(void)
+{	
+	mmHookInit();
+	printk("car start work!\n");
+	return 0;
+}
+
+
+static void mm_exit(void)
+{
+	msleep(1000);
+	mmHookExit();
+	mmDebug(MM_INFO,"xmit_Exit run..\n");
+}
+
+module_init(mm_init);
+module_exit(mm_exit);
---
+++ source.c
@@ -0,0 +1,19 @@
+#include"main.h"
+unsigned int preRoutHookDisp(void *priv, struct sk_buff *skb, 
+				 const struct nf_hook_state *state)
+{
+	struct udphdr *udph;
+	unsigned short innerDstPort;
+	static int seqNo = 0;
+    udph =(struct udphdr *)(skb->data+sizeof(struct iphdr));
+    innerDstPort=ntohs(udph->dest);
+
+    if (innerDstPort == UDP_SERV_TEST_PORT)   //处理特定端口
+    {
+		printk("get udp packet for port UDP_SERV_TEST_PORT\n");
+		kfree_skb(skb);
+		return NF_STOLEN;
+	}
+    else
+		return NF_ACCEPT;
+}
